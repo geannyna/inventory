72f70083089b70cd47e343a448af7dd2abb3c031
Claudia <claudia.iacob@hotmail.com>
2023-02-22 21:21:36 +0100

    Index page

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,5 +3,10 @@ import './bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('app')).render( <App />)
+ReactDOM.createRoot(document.getElementById('app')).render( 
+  <BrowserRouter>
+   <App />
+  </BrowserRouter>
+)
---
+++ resources/js/components/products/Index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Index page</h1>
+    </div>
+  )
+}
+export default Index
